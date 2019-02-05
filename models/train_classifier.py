07bac525668235cde028a845d38a118f92956b33
Hieu Nguyen <hieuqng26.email@gmail.com>
2019-02-04 22:10:12 -0600

    doc and chore: change build_model function and arguments

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import pickle
 import warnings
+import argparse
 
 # Text Processing
 import re
@@ -23,16 +24,44 @@ from nbsvm import NbSvmClassifier
 
 
 def load_data(database_filepath):
+    '''
+    Load data from sqlite database, quick cleaning, and
+    return the features and labels of the models.
+
+    INPUTS:
+        database_filepath: path to the sqlite database
+
+    OUTPUTS:
+        X, Y: features and labels of the model
+        Y.columns: categories of the label
+    '''
+
     engine = create_engine('sqlite:///' + database_filepath)
     df = pd.read_sql_table('DisasterResponse', engine)
+
+    # 'related' column has values of 0,1,2 which doesn't make sense for binary classification
     df['related'] = df['related'].replace(2, 1)
+
+    # 'child_alone' column has only value of 0. So our model will always predict 0.
+    df = df.drop('child_alone', axis=1)
+
     X = df['message']
     Y = df.drop(['id', 'message', 'genre', 'original'], axis=1)
-    Y.drop('child_alone', axis=1, inplace=True)
+
     return X, Y, Y.columns
 
 
 def tokenize(text):
+    '''
+    Preprocess text features by tokenization and lemmatization.
+
+    INPUTS:
+        text: string of text need to be processed
+
+    OUTPUTS:
+        tokens: a list of tokens from the text
+    '''
+
     # normalize case and remove punctuation
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
 
@@ -46,74 +75,103 @@ def tokenize(text):
     return tokens
 
 
-def scorer(y_test, y_pred, metric):
+def scorer(y_test, y_pred):
+    '''
+    Create a evaluation metric for the grid search.
+    '''
+
     report = classification_report(y_test, y_pred, output_dict=True)
     weighted_avg = report['weighted avg']
-    return weighted_avg[metric]
+    return weighted_avg['f1-score']
 
 
-def build_model(model_filepath=None):
+def build_model(pretrained_model):
     '''
     Build ML pipeline to including text processing and multi-output multi-class classifier
-    If tuning=True, output model is a grid search, which takes longer to train.
+    If pretrained_model not None, load pretrained model. Otherwise output model is a grid search, which takes longer to train.
     '''
 
-    if model_filepath != None:
-        clf = pickle.load(open(model_filepath, 'rb'))
-
+    if pretrained_model != None:
+        clf = pickle.load(open(pretrained_model, 'rb'))
     else:
-        clf = Pipeline([
-            ('vect', CountVectorizer(tokenizer=tokenize, ngram_range=(1, 2))),
+        pipeline = Pipeline([
+            ('countvec', CountVectorizer(tokenizer=tokenize)),
             ('tfidf', TfidfTransformer()),
-            ('clf', MultiOutputClassifier(NbSvmClassifier(C=10.0))),
+            ('clf', MultiOutputClassifier(NbSvmClassifier()))
         ])
+
+        parameters = {
+            'clf__estimator__C': [1.0, 5.0, 10.0],
+            'countvec__ngram_range': [(1, 1), (1, 2)]
+        }
+
+        f1_scorer = make_scorer(scorer)
+
+        # optimize model
+        clf = GridSearchCV(pipeline, parameters, scoring=f1_scorer,
+                           cv=3, verbose=10)
 
     return clf
 
 
-def evaluate_model(model, X_test, Y_test, category_names, verbose=True):
+def evaluate_model(model, X_test, Y_test, category_names, pretrained_model):
+    '''
+    Evaluate the classifier by classification report (sklearn).
+    If the pretrained_model is None, we trained the grid search, so best parameters will be reported.
+    '''
+    if pretrained_model == None:
+        print("Best parameters: ", model.best_params_)
+
     Y_pred = model.predict(X_test)
     report = classification_report(Y_test, Y_pred, target_names=category_names, output_dict=True)
 
-    if verbose:
-        print(classification_report(Y_test, Y_pred, target_names=category_names))
+    print("Validation Results: ")
+    print(classification_report(Y_test, Y_pred, target_names=category_names))
 
     return (Y_pred, report)
 
 
 def save_model(model, model_filepath):
+    '''
+    Save the model to a path specified by model_filepath.
+    '''
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
-    if len(sys.argv) <= 4:
-        # Ignore warnings
-        warnings.simplefilter('ignore')
+    # Ignore warnings
+    warnings.simplefilter('ignore')
 
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Training Disaster Message Classifier')
+    parser.add_argument('database_filepath', action='store', help='Directory of database file')
+    parser.add_argument('model_filepath', action='store', help='Path to save the model')
+    parser.add_argument('--pretrain', action='store', dest='pretrained_model',
+                        help='Path to pretrained model')
 
-        print('Building model...')
-        model = build_model()
+    args = parser.parse_args()
+    database_filepath = args.database_filepath
+    model_filepath = args.model_filepath
+    pretrained_model = args.pretrained_model
 
-        print('Training model...')
-        model.fit(X_train, Y_train)
+    # Train and Evaluate model
+    print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+    X, Y, category_names = load_data(database_filepath)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-        print('Evaluating model...')
-        Y_pred, report = evaluate_model(model, X_test, Y_test, category_names)
+    print('Building model...')
+    model = build_model(pretrained_model)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+    print('Training model...')
+    model.fit(X_train, Y_train)
 
-        print('Trained model saved!')
+    print('Evaluating model...')
+    Y_pred, report = evaluate_model(model, X_test, Y_test, category_names, pretrained_model)
 
-    else:
-        print('Please provide the filepath of the disaster messages database '
-              'as the first argument and the filepath of the pickle file to '
-              'save the model to as the second argument. \n\nExample: python '
-              'train_classifier.py ../data/DisasterResponse.db model_name.pkl')
+    print('Saving model...\n    MODEL: {}'.format(model_filepath))
+    save_model(model, model_filepath)
+
+    print('Trained model saved!')
 
 
 if __name__ == '__main__':
